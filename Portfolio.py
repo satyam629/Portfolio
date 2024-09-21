97b12425de129cb0711f72e66d19daaabe995be5
Satyam <63797763+satyam629@users.noreply.github.com>
2024-09-21 23:21:39 +0530

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Satyam - Data Engineer Portfolio", layout="wide")
 
 # Image URLs
 aws_logo_url = "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg"
-snowflake_logo_url = "https://upload.wikimedia.org/wikipedia/en/e/e9/Snowflake_Logo.svg"
+snowflake_logo_url = "https://upload.wikimedia.org/wikipedia/commons/f/ff/Snowflake_Logo.svg"
 pyspark_logo_url = "https://upload.wikimedia.org/wikipedia/commons/f/f3/Apache_Spark_logo.svg"
 
 # Background Style
